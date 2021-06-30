5ddb5504c6a5614e78d3b752f4197019320e0c29
Deveshb15 <deveshb583@gmail.com>
2021-06-30 19:02:37 +0530

    Basic scene Done

---
+++ 03-basic-scene/script.js
@@ -0,0 +1,30 @@
+// Scene
+const scene = new THREE.Scene()
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000})
+const cube = new THREE.Mesh(geometry, material)
+
+// Add cube to the scene
+scene.add(cube)
+
+// Sizes(aspect ratio)
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
