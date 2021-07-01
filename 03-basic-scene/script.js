5c9fa1951ccf95c76b61bc609bfa7533fe78e393
Deveshb15 <deveshb583@gmail.com>
2021-07-01 17:08:27 +0530

    added better comments

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,7 +1,7 @@
 // Scene
 const scene = new THREE.Scene()
 
-// Red Cube
+// Mesh - Red Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000})
 const cube = new THREE.Mesh(geometry, material)
