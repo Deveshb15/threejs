90423e73a817bbed168bb9fde4420f360e2ccbb4
Deveshb15 <deveshb583@gmail.com>
2021-07-02 12:55:40 +0530

    Animations done

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,4 +30,49 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+
+/**
+ * Animations
+ */
+
+// Time
+// let time = Date.now()
+let clock = new THREE.Clock()
+
+gsap.to(mesh.position, { duration:1, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration:1, delay: 2, x: 0 })
+
+const tick = () => {
+
+    // Time 
+    // const currentTime = Date.now()
+    // const deltaTime = currentTime - time
+    // time = currentTime
+    // const elapsedTime = clock.getElapsedTime()
+    // console.log(elapsedTime);
+
+    // console.log(deltaTime);
+
+    // Update Objects
+    // Position
+    // mesh.position.x += 0.01
+    // mesh.position.y += 0.001
+    // mesh.position.z += 0.01
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime)
+    // mesh.position.x = Math.tan(elapsedTime)
+    // mesh.position.y = Math.tan(elapsedTime)
+    
+    // Rotation
+    // mesh.rotation.x = elapsedTime
+    // mesh.rotation.y = elapsedTime
+    // mesh.rotation.z = elapsedTime
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
